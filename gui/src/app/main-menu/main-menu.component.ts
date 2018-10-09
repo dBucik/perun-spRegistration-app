539a761e3de0e3ded5d8568ed2a1441092ec8b12
vojtech-sassmann <vojtech.sassmann@gmail.com>
2018-10-09 11:30:59 +0200

    TODO

--- gui/src/app/main-menu/main-menu.component.ts
+++ gui/src/app/main-menu/main-menu.component.ts
@@ -1,41 +1,16 @@
-import {Component, OnInit} from '@angular/core';
-import {RequestsService} from "../core/services/requests.service";
-import {FacilitiesService} from "../core/services/facilities.service";
-import {Request} from "../core/models/Request";
-import {RequestStatus} from "../core/models/RequestStatus";
-import {RequestAction} from "../core/models/RequestAction";
-import {Facility} from "../core/models/Facility";
-import {MenuButtonComponent} from "./menu-button/menu-button.component";
-import {faDatabase, faEnvelopeOpen, IconDefinition} from "@fortawesome/free-solid-svg-icons";
+import {Component} from '@angular/core';
+import {faDatabase, faEnvelopeOpen } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.scss']
 })
-export class MainMenuComponent implements OnInit {
+export class MainMenuComponent {
 
-  menuButtons = [
-    {
-      icon: faEnvelopeOpen,
-      text: 'My requests',
-      route: 'requests'
-    },
-    {
-      icon: faDatabase,
-      text: 'My facilities',
-      route: 'facilities'
-    }
-  ];
+  requestsIcon = faEnvelopeOpen;
+  facilitiesIcon = faDatabase;
 
   constructor() { }
 
-  ngOnInit() {
-  }
-}
-
-interface MenuButton {
-  icon: IconDefinition,
-  text: string,
-  route: string;
 }
--- gui/src/app/shared/shared.module.ts
+++ gui/src/app/shared/shared.module.ts
@@ -1,19 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SortableTableDirective } from './layout/sortable-table.directive';
-import {SortableColumnComponent} from "./layout/sortable-column.component";
+import { SortableColumnComponent } from "./layout/sortable-column.component";
+import { TranslateModule } from '@ngx-translate/core';
+import { PerunFooterComponent } from './layout/perun-footer/perun-footer.component';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { PerunHeaderComponent } from "./layout/perun-header/perun-header.component";
+import { PerunSidebarComponent } from "./layout/perun-sidebar/perun-sidebar.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule,
+    CommonModule,
+    FontAwesomeModule
   ],
   declarations: [
     SortableColumnComponent,
-    SortableTableDirective
+    SortableTableDirective,
+    PerunHeaderComponent,
+    PerunSidebarComponent,
+    PerunFooterComponent
   ],
   exports: [
+    FontAwesomeModule,
     SortableColumnComponent,
-    SortableTableDirective
+    SortableTableDirective,
+    TranslateModule,
+    PerunHeaderComponent,
+    PerunSidebarComponent,
+    PerunFooterComponent
   ]
 })
 export class SharedModule { }
