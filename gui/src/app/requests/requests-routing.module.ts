9703e9e2d570f4cea455a9d07cb02509fd0ac10f
DanoFecko <dano9500@gmail.com>
2018-10-19 11:04:30 +0200

    Basic layout for newRequest page

--- gui/src/app/requests/requests-routing.module.ts
+++ gui/src/app/requests/requests-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {RequestsOverviewComponent} from "./requests-overview/requests-overview.component";
+import {NewRequestComponent} from "./new-request/new-request.component";
 
 const routes: Routes = [
   {
     path: '',
     component: RequestsOverviewComponent
+  },
+  {
+    path: 'new',
+    component: NewRequestComponent
   }
 ];
 
--- gui/src/app/requests/requests.module.ts
+++ gui/src/app/requests/requests.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { RequestsRoutingModule } from './requests-routing.module';
 import { RequestsOverviewComponent } from "./requests-overview/requests-overview.component";
 import {SharedModule} from "../shared/shared.module";
+import { NewRequestComponent } from './new-request/new-request.component';
 
 @NgModule({
   imports: [
@@ -11,6 +12,6 @@ import {SharedModule} from "../shared/shared.module";
     RequestsRoutingModule,
     SharedModule
   ],
-  declarations: [RequestsOverviewComponent]
+  declarations: [RequestsOverviewComponent, NewRequestComponent]
 })
 export class RequestsModule { }
