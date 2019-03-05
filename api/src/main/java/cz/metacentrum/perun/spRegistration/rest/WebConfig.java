e0c81a0f8c6975e782b41b2d1b2bf69c842c4894
Dominik Frantisek Bucik <bucik@ics.muni.cz>
2019-03-05 16:59:09 +0100

    Fixed missing attribute names
    Signed-off-by: Dominik Frantisek Bucik <bucik@ics.muni.cz>

--- api/src/main/java/cz/metacentrum/perun/spRegistration/rest/WebConfig.java
+++ api/src/main/java/cz/metacentrum/perun/spRegistration/rest/WebConfig.java
@@ -1,6 +1,5 @@
 package cz.metacentrum.perun.spRegistration.rest;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -9,12 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
  *
  * @author Vojtech Sassmann <vojtech.sassmann@gmail.com>
  */
-@Configuration
+//@Configuration
 public class WebConfig extends WebMvcConfigurationSupport  {
 	@Override
 	protected void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins("http://localhost:4200/spreg/")
+				.allowedOrigins("http://localhost:4200/spreg")
 				.allowedMethods(
 						"GET",
 						"POST",
