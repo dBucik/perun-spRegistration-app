6ab63c6a1e945bcf8973c7e4ed9ebf64beddedf6
Dominik František Bučík <bucik@ics.muni.cz>
2020-09-18 12:00:36 +0200

    Fixed not updating modified_by

--- api/src/main/java/cz/metacentrum/perun/spRegistration/service/impl/RequestsServiceImpl.java
+++ api/src/main/java/cz/metacentrum/perun/spRegistration/service/impl/RequestsServiceImpl.java
@@ -49,7 +49,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -413,6 +412,7 @@ public class RequestsServiceImpl implements RequestsService {
 
         request.setStatus(RequestStatus.APPROVED);
         request.setModifiedAt(new Timestamp(System.currentTimeMillis()));
+        request.setModifiedBy(userId);
         request.updateAttributes(new ArrayList<>(), false, appConfig);
 
         boolean requestProcessed = processApprovedRequest(request);
@@ -458,6 +458,7 @@ public class RequestsServiceImpl implements RequestsService {
 
         request.setStatus(RequestStatus.REJECTED);
         request.setModifiedAt(new Timestamp(System.currentTimeMillis()));
+        request.setModifiedBy(userId);
 
         boolean requestUpdated = requestManager.updateRequest(request);
         mailsService.notifyUser(request, MailsServiceImpl.REQUEST_STATUS_UPDATED);
@@ -498,6 +499,7 @@ public class RequestsServiceImpl implements RequestsService {
 
         request.updateAttributes(attributes, false, appConfig);
         request.setStatus(RequestStatus.WAITING_FOR_CHANGES);
+        request.setModifiedBy(userId);
         request.setModifiedAt(new Timestamp(System.currentTimeMillis()));
 
         boolean requestUpdated = requestManager.updateRequest(request);
