3529c79f08d0cf7aefe7f235d867501ec7dfe2ac
Dominik František Bučík <bucik@ics.muni.cz>
2020-11-13 07:36:09 +0100

    Fixed comparing arr values in items

--- gui/src/app/core/models/items/DetailViewItem.ts
+++ gui/src/app/core/models/items/DetailViewItem.ts
@@ -1,6 +1,7 @@
 import {PerunAttribute} from '../PerunAttribute';
 
 export class DetailViewItem {
+
   constructor(attr: PerunAttribute) {
     if (!attr) {
       return;
@@ -10,35 +11,31 @@ export class DetailViewItem {
     this.value = attr.value;
     this.oldValue = attr.hasOwnProperty('oldValue') ? attr.oldValue : null;
     this.comment = attr.hasOwnProperty('comment') ? attr.comment : null;
-
+    this.type = attr.definition.type;
     this.name = new Map<string, string>();
     this.description = new Map<string, string>();
 
     if (attr.input) {
-      this.position = attr.input.displayPosition;
-
       if (attr.input.displayName) {
-        this.name.set('en', attr.input.displayName.get('en'));
-        if (attr.input.displayName.get('cs')) {
-          this.name.set('cs', attr.input.displayName.get('cs'));
-        }
+        attr.input.displayName.forEach((value: string, key: string) => {
+          this.name.set(key.toLowerCase(), value);
+        });
       } else {
         this.name.set('en', attr.definition.displayName);
       }
-
       if (attr.input.description) {
-        this.description.set('en', attr.input.description.get('en'));
-        if (attr.input.displayName.get('cs')) {
-          this.description.set('cs', attr.input.description.get('cs'));
-        }
+        attr.input.description.forEach((value: string, key: string) => {
+          this.description.set(key.toLowerCase(), value);
+        });
       } else {
         this.description.set('en', attr.definition.description);
       }
+      this.position = attr.input.displayPosition;
     } else {
+      this.name.set('en', attr.definition.displayName);
+      this.description.set('en', attr.definition.description);
       this.position = 0;
     }
-
-    this.type = attr.definition.type;
   }
 
   urn: string;
@@ -65,27 +62,7 @@ export class DetailViewItem {
       case 'java.util.LargeArrayList':
         return !DetailViewItem.arrayValuesAreEqual(this.value, this.oldValue);
       case 'java.util.LinkedHashMap':
-        return !DetailViewItem.mapValuesEqual(this.value, this.oldValue);
-    }
-    return false;
-  }
-
-  public hasOldValue(): boolean {
-    switch (this.type) {
-      case 'java.lang.Boolean': {
-        return this.oldValue !== undefined
-          && this.oldValue !== null
-          && this.oldValue !== this.value;
-      }
-      case 'java.lang.String':
-      case 'java.lang.LargeString' :
-      case 'java.lang.Integer':
-        return this.oldValue !== this.value;
-      case 'java.util.ArrayList':
-      case 'java.util.LargeArrayList':
-        return !DetailViewItem.arrayValuesAreEqual(this.value, this.oldValue);
-      case 'java.util.LinkedHashMap':
-        return !DetailViewItem.mapValuesEqual(this.value, this.oldValue);
+        return !DetailViewItem.mapValuesAreEqual(this.value, this.oldValue);
     }
     return false;
   }
@@ -95,48 +72,42 @@ export class DetailViewItem {
       return v2 === undefined;
     } else if (v1 === null) {
       return v2 === null;
-    }
-
-    if (v2 === undefined || v2 === null) {
+    } else if (v2 === undefined || v2 === null) {
+      return false;
+    } else if (v1.length !== v2.length) {
       return false;
     }
 
-    if (v1.length !== v2.length) {
-      return false;
-    }
-
-    v1.sort().forEach(el => {
-      if (v2.indexOf(el) === -1) {
+    v1 = v1.sort();
+    v2 = v2.sort();
+    for (let i = 0; i < v1.length; i++) {
+      if (v1[i] !== v2[i]) {
         return false;
       }
-    });
+    }
     return true;
   }
 
-  private static mapValuesEqual(v1: Map<string, string>, v2: Map<string, string>) {
+  private static mapValuesAreEqual(v1: Object, v2: Object) {
     if (v1 === undefined) {
       return v2 === undefined;
     } else if (v1 === null) {
       return v2 === null;
-    }
-
-    if (v2 === undefined || v2 === null) {
+    } else if (v2 === undefined || v2 === null) {
+      return false;
+    } else if (Object.keys(v1).length !== Object.keys(v2).length) {
       return false;
     }
 
-    if (Object.keys(v1).length !== Object.keys(v2).length) {
-      return false;
-    }
-
-    Object.keys(v1).forEach(function (key) {
+    Object.keys(v1).forEach(function(key) {
       const value = v1[key];
       if (!v2.hasOwnProperty(key)) {
         return false;
       } else if (value !== v2[key]) {
         return false;
       }
-      return true;
     });
+    return true;
   }
 
 }
